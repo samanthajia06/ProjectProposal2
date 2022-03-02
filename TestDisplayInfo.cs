1abfb33a37d6ea978fc0e03c98a2604f771367b3
samantha solomon <ssolomon2024@chadwickschool.org>
2022-03-01 18:44:55 -0800

    finished last test

--- TestDisplayInfo.cs
+++ TestDisplayInfo.cs
@@ -8,10 +8,15 @@ namespace SSolomon_ProjectProposal2
         {
             Console.WriteLine("Calling DisplayInfo(). This will test how accurate your word is.");
 
-            Console.Write("You should see");
+            Console.Write("You should see green letters except for a red 'c' and a yellow 'k'");
             program.DisplayInfo("snack", "snake");
 
-            // TODO: write a few more of these
+            Console.Write("You should see green letters except for a white 'h'");
+            program.DisplayInfo("white", "write");
+
+            Console.Write("You should see green letters except for a red 'h', a yellow 'e', and white 'g', 'h', and 't'");
+            program.DisplayInfo("height", "sprite");
+
         }
     }
 }
